ecc6401ea4b6ec514d02a4c986457dce3da7f98f
JSquinatoa <jsquinatoa@uce.edu.ec>
2025-07-10 01:43:26 -0500

    Correción del boton de logueo

--- src/client/UvirtualClient.js
+++ src/client/UvirtualClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const guardarUsuario = async (nombre, password) => {
     try {
-        const response = await axios.post("http://localhost:8081/api/university/v1/students", {
+        const response = await axios.post("https://cripto-api-xv5i.onrender.com//api/university/v1/students", {
             username: nombre,
             password: password
         });
